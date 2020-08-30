fa61eca5de0d122955141b3ee5db5004375d0575
khontharos <khontharos.bb@gmail.com>
2020-08-30 12:24:59 +0700

    Updates

---
+++ precache-manifest.25a7f3f5ea4a80b8e71f0f31c6019dee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f27f1209f90d79c3afdd66eae80401",
+    "url": "/businessweb/index.html"
+  },
+  {
+    "revision": "9d8d4421908e7234299f",
+    "url": "/businessweb/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b2938aa76e051422504f",
+    "url": "/businessweb/static/css/main.16d53814.chunk.css"
+  },
+  {
+    "revision": "9d8d4421908e7234299f",
+    "url": "/businessweb/static/js/2.cefdc174.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/businessweb/static/js/2.cefdc174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2938aa76e051422504f",
+    "url": "/businessweb/static/js/main.2ebf5041.chunk.js"
+  },
+  {
+    "revision": "7ea5dca0db2f9abf75f3",
+    "url": "/businessweb/static/js/runtime-main.3a0ae10c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/businessweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
